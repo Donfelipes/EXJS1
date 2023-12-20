be7f49bc10fb20087fb10226d02deb5f8b877190
Donfelipes <donfelipes@MacBook-Air-de-Felipe.local>
2023-12-20 17:23:25 -0300

    2 commit

--- index.js
+++ index.js
@@ -71,11 +71,20 @@
 //     }
 // };
 
-#6 FizzBuzz
+// #6 FizzBuzz
 
 const fizzBuzz = (numero) => {
-    
-}
+    if(numero % 3 === 0 && numero % 5 === 0){
+        return "fizzbuzz"
+    } else if (numero % 3 === 0){
+        return "fizz"
+    } else if (numero % 5 === 0){
+        return "buzz"
+    } else {
+        return numero
+    }
+
+};
 
 
 
